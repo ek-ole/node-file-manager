416e89086faf07626cdb2b9d4250b156d1ed9cc1
ek-ole <ek_34@mail.ru>
2025-10-31 18:37:30 +0300

    feat: create index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const args = process.argv.slice(2);
+const usernameArg = args.find(arg => arg.startsWith('--username='));
+const username = usernameArg ? usernameArg.split('=')[1] : 'Anonym';
+
+console.log(`Hello, ${username}`)
