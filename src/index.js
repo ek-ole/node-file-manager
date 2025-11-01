510700753339836d1a11727c5ff6a10694ad8e5f
ek-ole <ek_34@mail.ru>
2025-11-01 18:49:25 +0300

    feat: implement input and output

--- src/index.js
+++ src/index.js
@@ -2,4 +2,17 @@ const args = process.argv.slice(2);
 const usernameArg = args.find(arg => arg.startsWith('--username='));
 const username = usernameArg ? usernameArg.split('=')[1] : 'Anonym';
 
-console.log(`Hello, ${username}`)
+console.log(`Welcome to the File Manager, ${username}!`);
+
+process.stdin.on('data', (data) => {
+  const input = data.toString().trim()
+  if (input === '.exit') {
+    console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+    process.exit(0)
+  }
+})
+
+process.on('SIGINT', () => { 
+    console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+    process.exit(0);
+});
