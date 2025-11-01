4ed1372216a2607959b53cf742522f2b47b0ea9c
ek-ole <ek_34@mail.ru>
2025-11-01 19:01:37 +0300

    feat: add current directory output

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import { cwd } from 'node:process';
+
 const args = process.argv.slice(2);
 const usernameArg = args.find(arg => arg.startsWith('--username='));
 const username = usernameArg ? usernameArg.split('=')[1] : 'Anonym';
 
 console.log(`Welcome to the File Manager, ${username}!`);
+console.log(`You are currently in ${cwd()}!`);
 
 process.stdin.on('data', (data) => {
   const input = data.toString().trim()
