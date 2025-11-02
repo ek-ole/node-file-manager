3ab40874df5daaebc7b512e978effbc6f4b6db73
ek-ole <ek_34@mail.ru>
2025-11-03 01:48:10 +0300

    refactor: add command router

---
+++ src/cli/commandRouter.js
@@ -0,0 +1,41 @@
+import { handleUp, handleCd, handleLs } from "../commands/navigation.js";
+import {
+  handleAdd,
+  handleMkdir,
+  handleCat,
+  handleRn,
+} from "../commands/files.js";
+import {
+  CD,
+  UP,
+  LS,
+  ADD,
+  MKDIR,
+  CAT,
+  RN,
+  INVALID_INPUT,
+} from "../utils/constants.js";
+
+const commandHandlers = {
+  [UP]: () => handleUp(),
+  [LS]: () => handleLs(),
+  [CD]: (args) => handleCd(args[0]),
+  [ADD]: (args) => handleAdd(args[0]),
+  [MKDIR]: (args) => handleMkdir(args[0]),
+  [CAT]: (args) => handleCat(args[0]),
+  [RN]: (args) => args.length === 2 ? handleRn(args[0], args[1]) : console.log(INVALID_INPUT),
+};
+
+export const routeCommand = (input) => {
+  for (const [prefix, handler] of Object.entries(commandHandlers)) {
+    if (input === prefix || input.startsWith(prefix)) {
+      const args = input
+        .slice(prefix.length)
+        .trim()
+        .split(" ")
+        .filter((arg) => arg);
+      return handler(args);
+    }
+  }
+  return console.log(INVALID_INPUT);
+};
--- src/cli/prompt.js
+++ src/cli/prompt.js
@@ -7,6 +7,7 @@ import {
 import { handleCd, handleUp, handleLs } from "../commands/navigation.js";
 import { CD, CURRENT_DIR, EXIT, INVALID_INPUT, UP, LS, ADD, MKDIR, CAT, RN } from "../utils/constants.js";
 import { cwd } from "node:process";
+import { routeCommand } from "./commandRouter.js";
 
 export const setupPrompt = (onExit) => {
   process.stdout.write('>');
@@ -18,34 +19,9 @@ export const setupPrompt = (onExit) => {
       onExit();
       return;
     } 
-    
-    if (input === UP) {
-      handleUp();
-    } else if (input.startsWith(CD)) {
-      const path = input.slice(CD.length).trim();
-      await handleCd(path);
-    } else if (input === LS) {
-      await handleLs();
-    } else if (input.startsWith(ADD)) {
-      const filename = input.slice(ADD.length).trim();
-      await handleAdd(filename);
-    } else if (input.startsWith(MKDIR)) {
-      const dirname = input.slice(MKDIR.length).trim();
-      await handleMkdir(dirname);
-    } else if (input.startsWith(CAT)) {
-      const filepath = input.slice(CAT.length).trim();
-      await handleCat(filepath);
-    } else if (input.startsWith(RN)) {
-      const args = input.slice(RN.length).trim().split(' ');   
-      if (args.length === 2) {
-        await handleRn(args[0], args[1]);
-      } else {
-        console.log(INVALID_INPUT);
-      }   
-    } else {
-      console.log(INVALID_INPUT);
-    }
-  
+
+    await routeCommand(input);
+      
     console.log(CURRENT_DIR(cwd()));  
     process.stdout.write(">");
   })
